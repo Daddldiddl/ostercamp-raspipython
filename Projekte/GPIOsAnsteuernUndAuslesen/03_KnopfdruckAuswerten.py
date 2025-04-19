a0fde5692cd7b9723245a0f421dac7b859f8b2bc
Jochen Brinkmann <brinkj@gmx.de>
2025-04-19 07:11:16 +0200

    Kleine Skriptanpassungen

--- Projekte/GPIOsAnsteuernUndAuslesen/03_KnopfdruckAuswerten.py
+++ Projekte/GPIOsAnsteuernUndAuslesen/03_KnopfdruckAuswerten.py
@@ -1,22 +1,35 @@
-# -------------------------------------------------------
-# LED über Knopf schalten, prüft ca. 20x pro Sekunde
-# -------------------------------------------------------
+# ------------------------------------------------------
+# LED per Knopf schalten
+# ------------------------------------------------------
 
 # Zusatzfunktionen laden
-import machine   # die für die Raspi-Ansteuerung nötigen Spezialfunktionen
-import time      # Funktionen rund um Zeit - für das Warten benötigt
+import machine   # Raspi Pico Funktionen
+import time      # Funktionen rund um Zeit
 
-# Weist GPIO2 als Ausgabe der Variablen led1 zu
-led1 = machine.Pin(2, machine.Pin.OUT)
+# Weist GPIO25 als Ausgabe der Variablen ledIntern zu
+ledIntern = machine.Pin(25, machine.Pin.OUT)
 
-# Weist GPIO0 als Eingang mit internem PULLDOWN-Widerstand der Variablen taster1 zu
-taster1 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
+# Weist GPIO15 als Ausgabe der Variablen ledStatus zu
+ledStatus = machine.Pin(15, machine.Pin.OUT)
+
+# Weist GPIO0 als Eingang der Variablen taster zu
+taster = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+alterTasterWert = 0
 
 # Endlosschleife
 while True:
-    if(taster1.value() == 1): # WENN der Taster gedrückt ist DANN
-        led1.value(1)         #     schalte die LED an
-    else:                     # ANSONSTEN
-        led1.value(0)         #     schalte die LED aus
-    time.sleep(0.05)          # schlafe 0.05 Sekunden (=50 Millisekunden)
-    # Ende der Schleife - jetzt wiederholen
+    # Aktuellen Wert des Tasters auslesen
+    tasterWert = taster.value()
+    # Wenn sich der Wert geändert hat, dann...
+    if (tasterWert != alterTasterWert):
+        # In Kommandozeile ausgeben
+        print("Taster jetzt {}".format(tasterWert))
+        # Status LED entsprechend schalten
+        ledStatus.value(tasterWert)
+        # den neuen Status merken
+        alterTasterWert = tasterWert
+    # halbe Sekunde warten
+    time.sleep(0.5)
+    # onboard LED toggeln - zeigt Programmaktivität
+    ledIntern.toggle()
--- StarterKit_Tutorial/MicroPython/MicroPython_Codes/Project 2/Button_Control_LED.py
+++ StarterKit_Tutorial/MicroPython/MicroPython_Codes/Project 2/Button_Control_LED.py
@@ -1,7 +1,8 @@
 import machine
 import time
-button = machine.Pin(16, machine.Pin.IN)
-led_onboard = machine.Pin(15, machine.Pin.OUT)
+button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+led_onboard = machine.Pin(25, machine.Pin.OUT)
+led_onboard.value(0)
 while True:
     if button.value() == 0:
         led_onboard.value(1)
